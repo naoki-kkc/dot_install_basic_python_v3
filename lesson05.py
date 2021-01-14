cf8861f6d84c79f592f6d7560d715c0a476b6811
naoki-kkc <naoki2kuchi@gmail.com>
2021-01-15 00:04:23 +0900

    complete 05

---
+++ lesson05.py
@@ -0,0 +1,15 @@
+# 算術
+x = 10
+print(x/4)
+print(x//4)
+print(x%4)
+print(x**2)
+
+# and/or, not
+print(True and False) # False
+print(True or False)  # True
+print(not True)       # False
+
+# 文字列結合
+print("Hello " + "world")
+print("Hi " * 3)
